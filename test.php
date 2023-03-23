c64b783f6e60fba8a76657805df00451e2bea0f7
eyupeminoglu <63679520+eyupeminoglu@users.noreply.github.com>
2023-03-23 16:21:58 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+$php = new PHPSC();
+$str = "TeSt";
+echo $php->reverse($str);
+echo $php->lowerCase($str);
+echo $php->upperCase($str);
+echo $php->firstLetterLowerCase($str);
+echo $php->capitalizeString($str);
+echo $php->length($str);
+
+?>
